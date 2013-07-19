1a34d01a2f3e6f0f9f2e46b66cd6422b291f77c2
snoj <josh@snoj.us>
2013-07-18 23:43:23 -0500

    Initial code commit
    Passes all tests found on Wikipedia's email address entry
      and then some

---
+++ tests/test-addresses.js
@@ -0,0 +1,38 @@
+var email = require('../index');
+var assert = require('assert');
+
+//good
+assert.ok(email.valid('email@example.com'));
+assert.ok(email.valid('email@example'));
+assert.ok(email.valid('email+sometag@example.com'));
+assert.ok(email.valid('"@somethingsomething"@example.com'));
+assert.equal(email.valid('"email@example.com'), false);
+assert.equal(email.valid('"email@example.com'), false);
+
+//bad
+assert.equal(email.valid('badip@[827.750.304.001]'), false);
+assert.equal(email.valid('badip@[827.750.304.001'), false);
+assert.equal(email.valid('bad@ThisHostAddressSpaceIsGreaterThanSixtyThreeCharactersLetsSeeHowManyICanFitInHereWithoutDestroyingTheWorld.com'), false);
+assert.equal(email.valid('bad@ThisAddressIsGreaterThanTwoHundredAndFiftyThreeCharactersLetsSeeHowManyICanFitInHereWithoutDestroyingTheWorld.com.Unfortunatly.we.were.not.quite.there.yet.but.we.getting.closer.by.the.minute.I.assure.you.we.are.almost.there.dot.some.new.top.level.domain.com.us.tz.kosher'), false);
+
+//shamelessly copied from http://en.wikipedia.org/wiki/Email_address
+assert.ok(email.valid('niceandsimple@example.com'));
+assert.ok(email.valid('very.common@example.com'));
+assert.ok(email.valid('a.little.lengthy.but.fine@dept.example.com'));
+assert.ok(email.valid('disposable.style.email.with+symbol@example.com'));
+assert.ok(email.valid('user@[2001:db8:1ff::a0b:dbd0]'));
+assert.ok(email.valid('"much.more unusual"@example.com'));
+assert.ok(email.valid('"very.unusual.@.unusual.com"@example.com'));
+assert.ok(email.valid('"very.(),:;<>[]\\".VERY.\\"very@\\\\ \\"very\\".unusual"@strange.example.com'));
+assert.ok(email.valid('postbox@com'));
+assert.ok(email.valid('admin@mailserver1'));
+assert.ok(email.valid('!#$%&\'*+-/=?^_`{}|~@example.org'));
+assert.ok(email.valid('"()<>[]:,;@\\"!#$%&\'*+-/=?^_`{}| ~.a"@example.org'));
+assert.ok(email.valid('" "@example.org'));
+
+assert.equal(email.valid("Abc.example.com"), false);
+assert.equal(email.valid("A@b@c@example.com"), false);
+assert.equal(email.valid('a"b(c)d,e:f;g<h>i[j\k]l@example.com'), false);
+assert.equal(email.valid('just"not"right@example.com'), false);
+assert.equal(email.valid('this is"not\allowed@example.com'), false);
+assert.equal(email.valid('this\ still\"not\\allowed@example.com'), false);
---
+++ tests/test-speed.js
@@ -0,0 +1,5 @@
+console.time("25 emails time ten thousand tries");
+for(var i = 0; i < 10000; i++) {
+  var tests = require('./test-addresses');
+}
+console.timeEnd("25 emails time ten thousand tries");
